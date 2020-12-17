f0ea30489a654f2ec5f15b0f3b7fbd970c5d196d
Egor Kurnev <kurnev.e@gmail.com>
2020-12-17 13:50:15 +0300

    Add core functionality

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+
+const globOptions = {
+  followSymbolicLinks:
+    core.getInput("follow-symbolic-links").toUpper() !== "FALSE",
+};
+
+try {
+  const filenames = core.getInput("filenames");
+  core.startGroup("Using filenames:\n");
+  for (const name of filenames) {
+    core.info(name + "\n");
+  }
+  core.endGroup();
+
+  const patterns = core.getInput("patterns");
+
+  core.startGroup("Using glob patterns:\n");
+  for (const pattern of patterns) {
+    core.info(pattern + "\n");
+  }
+  core.endGroup();
+
+  const globber = await glob.create(filenames, globOptions);
+  const matchingFiles = await globber.glob();
+
+  core.startGroup("Files that match the glob patterns:\n");
+  for (const file of matchingFiles) {
+    core.info(file + "\n");
+  }
+  core.endGroup();
+
+  if (matchingFiles.length > 0) {
+    core.setOutput("match", "true");
+  }
+
+  core.setOutput("files", matchingFiles);
+} catch (error) {
+  core.setFailed(error.message);
+}
